644900a1b14f5a7a36665e7f4bfbfa065fe9a642
bijay135 <bijayxshah@gmail.com>
2016-11-14 12:49:34 +0545

    Add files via upload
    Ask and print name

---
+++ Ask and Print Name.c
@@ -0,0 +1,18 @@
+/*
+ Author : Bijay Shah
+ Subject : Programming Fundmentals
+ Roll no :
+ Lab Sheet no : 2
+ Program : Write a program to ask and print name
+ Date : 14 November , 2016
+ */
+ #include<stdio.h>
+ #include<conio.h>
+ void main()
+ {
+  char name[10];
+  printf("what is your name\n");
+  scanf("%s",&name);
+  printf("name of user is %s",name);
+  getch();
+ }
