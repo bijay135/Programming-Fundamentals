a885e2e94943fbe9e4b94726026511b9184012eb
bijay135 <bijay.shah357@gmail.com>
2016-11-16 10:59:21 +0545

    Add files via upload
    print name

---
+++ Print Name.c
@@ -0,0 +1,20 @@
+/*
+Author/Student : Bijay Shah
+Subject : Programming Fundmentals
+Roll no :
+Lab Sheet no : 1
+Program : Write a C program to print your name
+Date: November 14 , 2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+
+printf("My name is Bijay");
+getch();
+
+}
+
+
