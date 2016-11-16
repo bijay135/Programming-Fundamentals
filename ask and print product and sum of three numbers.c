2eaf735761c258f5a4ccfbf5628b3cb5774d6c33
bijay135 <bijay.shah357@gmail.com>
2016-11-16 13:13:40 +0545

    ask and print product and sum of three numbers

---
+++ ask and print product and sum of three numbers.c
@@ -0,0 +1,26 @@
+/*
+Author : Bijay Shah
+Subject : Programming Fundamentals
+Roll No :
+Lab Sheet no : 2
+Program : Write a c program to print product & sum of three no. asked from user
+Date : 16 November , 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,sum,product;
+printf("Enter first number:\n");
+scanf("%d",&a);
+printf("Enter second number:\n");
+scanf("%d",&b);
+printf("Enter third number:\n");
+scanf("%d",&c);
+sum=a+b+c;
+printf("Sum of three no is: %d\n",sum);
+getch();
+product=a*b*c;
+printf("Product of three number is: %d",product);
+getch();
+}
