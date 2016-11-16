7658aa632848c292097084cf6645ce44701516d5
bijay135 <bijay.shah357@gmail.com>
2016-11-16 12:26:54 +0545

    ask and print division of two numbers

---
+++ ask and print division of two numbers.c
@@ -0,0 +1,20 @@
+/*
+Author : Bijay Shah
+Subject : Programming Fundmentals
+Roll no :
+Lab Sheet No : 2
+Program : Write a program to print and divide two no. asked from users
+*/
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ float a,b,division;
+ printf("Enter first number\n");
+ scanf("%f",&a);
+ printf("Enter second number\n");
+ scanf("%f",&b);
+ division=a/b;
+ printf("Division of two numbers is : %f",division);
+ getch();
+}
