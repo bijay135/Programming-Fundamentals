4525ef3fda0d542ad5a5391a07a88da12188e505
bijay135 <bijayshah975@gmail.com>
2016-11-18 13:00:31 +0545

    Add files via upload

---
+++ L3P3-ask and print area and circumference of circle.c
@@ -0,0 +1,23 @@
+/*
+Author : Bijay Shah
+Subject : Programming Fundamentals
+Roll no :
+Lab Sheet no : 3
+Program : Write a C program to ask and print area and circumference of circle
+Date: November 18 , 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+#define pi 3.14
+void main()
+{
+float r,area,circumference;
+printf("Enter radius of the circle:\n");
+scanf("%f",&r);
+area=pi*pow(r,2);
+printf("Area of circle is: %f\n",area);
+circumference=2*pi*r;
+printf("Circumference of circle is: %f",circumference);
+getch();
+}
