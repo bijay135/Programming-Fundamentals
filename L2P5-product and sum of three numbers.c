a2ee30b441cc094c8cdf43970b6f03dc56169564
bijay135 <bijay.shah975@gmail.com>
2016-11-25 11:10:00 +0545

    Update L2P5-product and sum of three numbers.c

--- L2P5-product and sum of three numbers.c
+++ L2P5-product and sum of three numbers.c
@@ -10,16 +10,16 @@ Date : 16 November , 2016
 #include<conio.h>
 void main()
 {
-int a,b,c,sum,product;
-printf("Enter first number:\n");
-scanf("%d",&a);
-printf("Enter second number:\n");
-scanf("%d",&b);
-printf("Enter third number:\n");
-scanf("%d",&c);
-sum=a+b+c;
-printf("Sum of three no is: %d\n",sum);
-product=a*b*c;
-printf("Product of three number is: %d",product);
-getch();
+  int a,b,c,sum,product;
+  printf("Enter first number:\n");
+  scanf("%d",&a);
+  printf("Enter second number:\n");
+  scanf("%d",&b);
+  printf("Enter third number:\n");
+  scanf("%d",&c);
+  sum=a+b+c;
+  printf("Sum of three no is: %d\n",sum);
+  product=a*b*c;
+  printf("Product of three number is: %d",product);
+  getch();
 }
