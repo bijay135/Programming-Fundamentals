a2f4061169bffbb3499e8e5b88e5ac5815c68417
bijay135 <bijay.shah975@gmail.com>
2016-11-25 11:15:51 +0545

    Update L5P4-find ASCII value.c

--- L5P4-find ASCII value.c
+++ L5P4-find ASCII value.c
@@ -11,12 +11,12 @@ Date: November 23 , 2016
 int main()
 {
 
-char character;
-int asciiValue;
-printf("Enter your character:\n");
-scanf("%c",&character);
-asciiValue=character;
-printf("The ASCII value of the character %c is : %d",character,asciiValue);
-getch();
-return 0;
+  char character;
+  int asciiValue;
+  printf("Enter your character:\n");
+  scanf("%c",&character);
+  asciiValue=character;
+  printf("The ASCII value of the character %c is : %d",character,asciiValue);
+  getch();
+  return 0;
 }
