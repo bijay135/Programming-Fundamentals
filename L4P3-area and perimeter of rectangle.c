b0e3d269fb391083de9415792dd134a3912ee313
bijay135 <bijay.shah975@gmail.com>
2016-11-25 11:55:05 +0545

    Update L4P3-area and perimeter of rectangle.c

--- L4P3-area and perimeter of rectangle.c
+++ L4P3-area and perimeter of rectangle.c
@@ -17,7 +17,7 @@ int main()
   scanf("%f",&b);
   area=l*b;
   printf("The area of the rectangle is : %f\n",area);
-  perimeter=2(l+b);
+  perimeter=2*(l+b);
   printf("The perimeter of the rectangle is : %f",perimeter);
   getch();
   return 0;
