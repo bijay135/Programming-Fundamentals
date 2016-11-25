c78d9b00624bf375b6c3a482bdb83e07c3a6fd68
bijay135 <bijay.shah975@gmail.com>
2016-11-25 11:09:30 +0545

    Update L2P4-area of triangle.c

--- L2P4-area of triangle.c
+++ L2P4-area of triangle.c
@@ -10,12 +10,12 @@ Date : 16 November , 2016
 #include<conio.h>
 void main()
 {
-float b,h,area;
-printf("Enter the base of the triangle:\n");
-scanf("%f",&b);
-printf("Enter the height of the triangle:\n");
-scanf("%f",&h);
-area=0.5*b*h;
-printf("The area of triangle is : %f",area);
-getch();
+  float b,h,area;
+  printf("Enter the base of the triangle:\n");
+  scanf("%f",&b);
+  printf("Enter the height of the triangle:\n");
+  scanf("%f",&h);
+  area=0.5*b*h;
+  printf("The area of triangle is : %f",area);
+  getch();
 }
