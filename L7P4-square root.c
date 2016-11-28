5414f6c6f736525eb6988229d63b0d052c7bb711
bijay135 <bijay.shah975@gmail.com>
2016-11-28 13:01:15 +0545

    Add files via upload

---
+++ L7P4-square root.c
@@ -0,0 +1,21 @@
+/*
+Author : Bijay Shah
+Subject : Programming Fundamentals
+Roll no :
+Lab Sheer NO : 7
+Program : Write a C Program to find square
+Date : 28 November , 2016
+*/
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+int main()
+{
+  int n,sr;
+  printf("Enter your number:\n");
+  scanf("%d",&n);
+  sr=sqrt(n);
+  printf("The square of your number is : %d",sr);
+  getch();
+  return 0;
+}
