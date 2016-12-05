1ec32e4f40270b5daa64990d5d4cb1735bcea13d
bijay135 <bijay.shah975@gmail.com>
2016-12-05 11:31:09 +0545

    Add files via upload

---
+++ L10P1-relations operations result.c
@@ -0,0 +1,44 @@
+/*
+Author : Bijay Shah
+Subject : Programming Fundamentals
+Roll no :
+Lab Sheet NO : 10
+Program : Write a C Program to use Relational operations
+Date : December 2 , 2016
+*/
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    float n;
+    printf("Enter your percentage:\n");
+    scanf("%f",&n);
+    if (n>=40)
+    {
+        printf("You have passed\n");
+        if (n>=80)
+        {
+            printf("Distinction\n");
+        }
+        if (n>=60)
+        {
+            printf("First Division\n");
+        }
+        if (n>=50)
+        {
+            printf("Second Division\n");
+        }
+        if (n>=40)
+        {
+            printf("Third Division\n");
+        }
+    }
+    if (n<35)
+    {
+        printf("You have failed");
+    }
+
+    getch();
+    return 0;
+}
