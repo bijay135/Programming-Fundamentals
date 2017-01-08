36717997eb4037a2b2f5de50d1b2ba3f702a68df
bijay135 <bijay.shah975@gmail.com>
2017-01-08 10:56:26 +0545

    Add files via upload

---
+++ L16P1-leap year or not using ternary.c
@@ -0,0 +1,22 @@
+/*
+Author : Bijay Shah
+Subject : Programming Fundamentals
+Roll no :
+Lab Sheet NO : 16
+Program :
+Date : January 8 , 2017
+*/
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n;
+    printf("Enter the year:\n");
+    scanf("%d",&n);
+    (n%100==0)?printf("The year is not a leap year"):
+    (n%4==0)?printf("The year is a leap year"):
+    (n%400==0)?printf("The year is a leap year"):
+     printf("The year is not a leap year");
+    getch();
+    return 0;
+}
