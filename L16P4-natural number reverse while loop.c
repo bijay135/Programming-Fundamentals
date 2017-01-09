9402ba943f6a7f502557223052548bce08ef9331
bijay135 <bijay.shah975@gmail.com>
2017-01-09 16:23:08 +0545

    Update L16P4-natural number reverse while loop.c

--- L16P4-natural number reverse while loop.c
+++ L16P4-natural number reverse while loop.c
@@ -3,7 +3,7 @@ Author : Bijay Shah
 Subject : Programming Fundamentals
 Roll no :
 Lab Sheet NO : 16
-Program :
+Program : WAP to display reverse natural number from n to 1 using while loop
 Date : January 8 , 2017
 */
 
