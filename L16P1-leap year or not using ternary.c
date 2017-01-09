158a93386318d2ed923bf0e7b42228b54c24a562
bijay135 <bijay.shah975@gmail.com>
2017-01-09 16:17:54 +0545

    Update L16P1-leap year or not using ternary.c

--- L16P1-leap year or not using ternary.c
+++ L16P1-leap year or not using ternary.c
@@ -3,7 +3,7 @@ Author : Bijay Shah
 Subject : Programming Fundamentals
 Roll no :
 Lab Sheet NO : 16
-Program :
+Program : WAP to check leap year using ternary
 Date : January 8 , 2017
 */
 #include<stdio.h>
