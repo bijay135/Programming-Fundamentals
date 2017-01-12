0aa7ce8a77a3454539e3b52ea37288051f8de5e6
bijay135 <bijay.shah975@gmail.com>
2017-01-12 13:12:47 +0545

    Add files via upload

---
+++ L17P8-product of digits while loop.c
@@ -0,0 +1,10 @@
+/*
+Author : Bijay Shah
+Subject : Programming Fundamentals
+Roll no :
+Lab Sheet NO : 17
+Program :WAP to calculate sum of digits
+Date : January 12 , 2017
+*/
+#include<stdio.h>
+#include<conio.h>
