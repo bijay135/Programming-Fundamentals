aaebb33fb4c543fec5bebce74d9cf1de69a1d9cd
bijay135 <bijay.shah975@gmail.com>
2017-01-19 13:03:32 +0545

    Add files via upload

---
+++ L20P1-function-HCF LCM.c
@@ -0,0 +1,10 @@
+/*
+Author : Bijay Shah
+Subject : Programming Fundamentals
+Roll no :
+Lab Sheet NO : 20
+Program :WAP to find HCF LCM using function
+Date : January 15 , 2017
+*/
+#include<stdio.h>
+#include<conio.h>
