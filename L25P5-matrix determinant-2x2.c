4436a37b18b1e472f5cf4b4b0f8b8e92ec6c89ba
bijay135 <bijay.shah975@gmail.com>
2017-01-31 13:03:18 +0545

    Add files via upload

---
+++ L25P5-matrix determinant-2x2.c
@@ -0,0 +1,8 @@
+/*
+Author : Bijay Shah
+Subject : Programming Fundamentals
+Roll no :
+Lab Sheet NO : 25
+Program :WAP to find determinant of 2x2 matrix
+Date: January 31 , 2017
+*/
---
+++ L25P6-matrix transpose-3x2.c
@@ -0,0 +1,8 @@
+/*
+Author : Bijay Shah
+Subject : Programming Fundamentals
+Roll no :
+Lab Sheet NO : 25
+Program :WAP to find transpose of 3x2 matrix
+Date: January 31 , 2017
+*/
