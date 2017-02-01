ae6b6174847fdcc38d8967b5457419c9b5a434c2
bijay135 <bijay.shah975@gmail.com>
2017-02-01 17:46:15 +0545

    Update L24P2-array ascending order.c

--- L24P2-array ascending order.c
+++ L24P2-array ascending order.c
@@ -25,7 +25,7 @@ int main()
         }
 
     }
-    printf("The array arranged in ascending order are:\n\n");
+    printf("The array in ascending order are:\n\n");
     for (i=0;i<10;i++)
     {
         printf("%d\t",a[i]);
